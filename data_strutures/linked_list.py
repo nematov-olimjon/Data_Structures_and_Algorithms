912ce1deaecc388d9e539983448c0c462678cc63
nematov-olimjon <nematov.ol2001@gmail.com>
2021-11-02 10:39:45 +0500

    Implemented adding node to Linked List

--- data_strutures/linked_list.py
+++ data_strutures/linked_list.py
@@ -16,7 +16,11 @@ class LinkedList:
     Single Linked List data structure
     """
 
-    def __init__(self, nodes):
+    def __init__(self, nodes=None):
+        """
+        Creating Linked List with given elements in nodes.
+        Nodes parameter can be any iterable object.
+        """
         self.head = None
         if nodes is not None:
             node = Node(nodes.pop(0))
@@ -39,3 +43,52 @@ class LinkedList:
         while node is not None:
             yield node
             node = node.next
+
+    def __len__(self):
+        count = 0
+        for current_node in self:
+            count += 1
+        return count
+
+    def add_first(self, data):
+        """
+        Insertion node with data parameter at the beginning of Linked List.
+        """
+        node = Node(data)
+        node.next = self.head
+        self.head = node
+
+    def add_last(self, data):
+        """
+        Insertion node with data parameter at the end of Linked List.
+        """
+        node = Node(data)
+        if self.head is None:
+            self.head = node
+        else:
+            for current_node in self:
+                pass
+            current_node.next = node
+
+    def add_between(self, position, data):
+        """
+        Insert node in given position. Insertion is similiar to the
+        logic of list.insert()
+        """
+        node = Node(data)
+        length_of_linked_list = len(self)
+        if position >= length_of_linked_list:
+            self.add_last(data)
+            return
+        elif position == 0 or (
+            position < 0 and abs(position) >= length_of_linked_list
+        ):  # noqa: E501
+            self.add_first(data)
+            return
+        count = 0
+        for current_node in self:
+            if count == abs(position) - 1:
+                break
+            count += 1
+        node.next = current_node.next
+        current_node.next = node
